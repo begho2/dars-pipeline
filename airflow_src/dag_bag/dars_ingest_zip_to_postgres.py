7d0775039f8db5735e5e92d186033a86b2c0eed7
Leo Edwards <leo.edwards@carnallfarrar.com>
2020-11-03 16:41:58 +0000

    adding s3 connection

--- airflow_src/dag_bag/dars_ingest_zip_to_postgres.py
+++ airflow_src/dag_bag/dars_ingest_zip_to_postgres.py
@@ -2,7 +2,10 @@ import os
 from datetime import timedelta
 
 from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
 from airflow.contrib.operators.spark_submit_operator import SparkSubmitOperator
+from airflow.operators.python_operator import PythonOperator
+from airflow.hooks.S3_hook import S3Hook
 from airflow.utils.dates import days_ago
 
 args = {
@@ -53,12 +56,25 @@ def create_spark_operator(task, class_name, filename, limit=100):
         # conf={'spark.memory.fraction': '0.1'},
     )
 
+def list_s3_keys():
+    # Initialize the s3 hook
+    s3_hook = S3Hook()
+    # Read the keys from s3 bucket
+    paths = s3_hook.list_keys(bucket_name='dars-raw', prefix='HES-AE')
+    s3_hook.get_key(key="HES-AE/NIC243790_HES_AE_201599.zip", bucket_name="dars-raw").download_file("./input_data/NIC243790_HES_AE_201599.zip")
+    print(paths)
+
+
 with dag:
     for filename in zip_files:
         key = filename.strip(".zip")
+        list_keys = PythonOperator(
+            task_id="list_s3_keys",
+            python_callable=list_s3_keys,
+        )
         setup_postgres_schema = create_spark_operator(task=f"SetupSchema", class_name='cf.ZipToPostgresSchemaMain' , filename=filename)
         push_to_postgres = create_spark_operator(task=f"InsertData", class_name='cf.ZipToPostgresInsertMain' , filename=filename, limit=100)
-        setup_postgres_schema >> push_to_postgres
+        list_keys >> setup_postgres_schema >> push_to_postgres
 
 
 
--- airflow_src/functions/parq_to_postgres_pyspark.py
+++ airflow_src/functions/parq_to_postgres_pyspark.py
@@ -232,20 +232,20 @@ if __name__ == "__main__":
     """
         Usage: pi [partitions]
     """
-    spark = SparkSession\
-        .builder\
-        .appName("PythonPi")\
-        .getOrCreate()
+    # spark = SparkSession\
+    #     .builder\
+    #     .appName("PythonPi")\
+    #     .getOrCreate()
 
-    partitions = int(sys.argv[1]) if len(sys.argv) > 1 else 2
-    n = 100000 * partitions
+    # partitions = int(sys.argv[1]) if len(sys.argv) > 1 else 2
+    # n = 100000 * partitions
 
-    def f(_):
-        x = random() * 2 - 1
-        y = random() * 2 - 1
-        return 1 if x ** 2 + y ** 2 <= 1 else 0
+    # def f(_):
+    #     x = random() * 2 - 1
+    #     y = random() * 2 - 1
+    #     return 1 if x ** 2 + y ** 2 <= 1 else 0
 
-    count = spark.sparkContext.parallelize(range(1, n + 1), partitions).map(f).reduce(add)
-    print("Pi is roughly %f" % (4.0 * count / n))
+    # count = spark.sparkContext.parallelize(range(1, n + 1), partitions).map(f).reduce(add)
+    # print("Pi is roughly %f" % (4.0 * count / n))
 
-    spark.stop()
+    # spark.stop()
